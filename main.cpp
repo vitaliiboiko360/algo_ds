e54addac974a23b9d03fd02f851bea23c8b901f3
vitalii boiko <vukelan@gmail.com>
2020-07-26 18:31:01 +0000

    test list

--- main.cpp
+++ main.cpp
@@ -12,10 +12,6 @@ rg gen(295);
 
 int main(int argc, char* argv[])
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> fadcf2d61710a700befbcbdb27ffcded8095ef46
     u_list v_nums;
     
     for(int i=0; i<10; i++)
@@ -31,40 +27,6 @@ int main(int argc, char* argv[])
        }
        v_nums.pop_back();
     }
-<<<<<<< HEAD
-=======
-=======
-    // u_array sz_collctn;
     
-    // for(int i=0; i<10; i++)
-    // {
-    //     sz_collctn.push_back(gen.get());
-    // }
-    
-    // std::cout<<"\n\n\n";    
-
-    // for(int i=0; i<10; i++)
-    // {
-    //     std::cout << sz_collctn.pop_back()  << ", ";
-    // }
-    // std::cout<<"\n";
-
-    // std::cout<<"\n\n\n";
-
-    std::vector<int> v_nums;
-
-    for(int i=0; i<10; i++)
-    {
-        v_nums.push_back(gen.get());
-    }
-
-    for(int i=0; i<11; i++)
-    {
-        std::cout<<"i= "<<i<<" "<<v_nums.back()<<"\n";
-        v_nums.pop_back();
-    }
->>>>>>> 66560a9227bcd8089a5fa67275feef0d80d74461
->>>>>>> fadcf2d61710a700befbcbdb27ffcded8095ef46
-
     return 0;
 }
