ed9046651948aeab7c7d7467e4023a210722d667
vitalii boiko <vukelan@gmail.com>
2020-07-27 19:14:51 +0000

    add push_front insert

--- list.cpp
+++ list.cpp
@@ -11,6 +11,17 @@ void u_list::push_back(int value)
     size++;
 }
 
+void u_list::push_front(int value)
+{
+    node* new_node = new node(value);
+    node* current_first = first;
+
+    first = new_node;
+    first->next = current_first;
+
+    size++;
+}
+
 void u_list::pop_back()
 {
     node* last_to_delete = last;
@@ -18,4 +29,16 @@ void u_list::pop_back()
 
     delete last_to_delete;
     size--;
+}
+
+void u_list::insert(node* pos, int value)
+{
+    node* new_node = new node(value);
+
+    new_node->prev = pos->prev;
+    new_node->next = pos;
+
+    pos->prev = new_node;
+       
+    size++;
 }
